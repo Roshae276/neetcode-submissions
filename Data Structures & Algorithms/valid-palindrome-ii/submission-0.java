24230d92763dfec5ec5acc16d85842bd182b2dcb
Roshni dodani <roshae276@gmail.com>
2026-07-08 21:33:06 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }else{
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+            }
+        }
+        return true;
+    }
+    boolean isPalindrome(String s , int left , int right){
+        while(left<right){
+            if(s.charAt(left) == s.charAt(right)){
+                left++;
+                right--;
+            }else return false;
+        }
+        return true;
+    }
+}
